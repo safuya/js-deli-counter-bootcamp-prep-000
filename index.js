45f04f02141c90a70971c38919b86f7a7690e5ea
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-13 09:46:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function takeANumber (katzDeliLine, name) {
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
     var nextCustomer = katzDeliLine.shift()
-    return `Currently serving ${nextCustomer}`
+    return `Currently serving ${nextCustomer}.`
   }
   return 'There is nobody waiting to be served!'
 }
